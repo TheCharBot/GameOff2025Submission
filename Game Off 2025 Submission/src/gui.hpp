911f18ff1f447610dd42ce1acf97d62b945a6d07
charles <charles@lpvondollen.family>
2025-11-08 07:22:15 -0800

    Started GUI
    started gui, put three working buttons on the bottom of screen.

---
+++ Game Off 2025 Submission/src/gui.hpp
@@ -0,0 +1,33 @@
+#ifndef GUI_HPP
+#define GUI_HPP
+
+#include "raylib.h"
+
+extern Texture2D all_elements;
+extern Rectangle elements[1];
+
+
+extern Vector2 default_rotation;
+
+extern void gui_init();
+extern void gui_update();
+
+class Button_bottom_trio{
+    public:
+    Rectangle button_1_rect;
+    Rectangle button_2_rect;
+    Rectangle button_3_rect;
+    Vector2 slot_1_position;
+    Vector2 slot_2_position;
+    Vector2 slot_3_position;
+    Rectangle scaled_sprite_1;
+    Rectangle scaled_sprite_2;
+    Rectangle scaled_sprite_3;
+    
+    void init();
+    int is_pressed();
+    void update();
+
+};
+
+#endif
---
+++ Game Off 2025 Submission/src/main.cpp
@@ -0,0 +1,61 @@
+// Wavebreak
+
+// enemies, large-scale td game
+
+// big screen td game, similar to worldbox in scale and sprites
+
+// waves of enemies visible
+
+// base-building, upgrades, simple pixel art
+
+// AAP-64 palette
+
+// Boss fight similar to undertale, but with tower defense as well
+
+// 6 waves,
+// the wave of light(green),
+// the wave of strength(pink),
+// the wave of speed(yellow),
+// the wave of time(brown),
+// the wave of evasion(red), and
+// the wave of defense(blue)
+
+// each wave has own attack
+
+// light: almost undodgable, low damage
+// strength: dodgable, but high damage
+// speed: undodgable, medium damage
+// time: buffs other waves with time powers, so save slotting, similar to with the flowey fight
+// evasion: allows boss and enemies to dodge your attacks
+// defense: blocks your attacks when active, decently easy to dodge
+#include <raylib.h>
+
+#include "gui.hpp"
+
+#include "config.hpp"
+int main()
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "WAVEBREAK");
+    Image icon = LoadImage("gfx/icon/icon.png");
+    SetWindowIcon(icon);
+    UnloadImage(icon);
+    SetTargetFPS(60);
+
+    gui_init();
+    Button_bottom_trio button;
+    button.init();
+    // Game Loop
+    while (WindowShouldClose() == false)
+    {
+        // Game Logic
+
+        BeginDrawing();
+        DrawRectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
+        
+        button.update();
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
