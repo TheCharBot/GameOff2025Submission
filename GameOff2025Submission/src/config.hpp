181c56c62ac4359747e7a3f784ff2f08878b7c83
charles <charles@lpvondollen.family>
2025-11-19 17:18:08 -0800

    Started Menu Implementation

---
+++ GameOff2025Submission/src/config.hpp
@@ -0,0 +1,31 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#include "raylib.h"
+#include <iostream>
+#include <utility>
+
+// Global-focused macros
+#define SCREEN_HEIGHT 800
+#define SCREEN_WIDTH 800
+
+//GUI-focused macros
+#define SLOT_1_X 309
+#define SLOT_2_X 373
+#define SLOT_3_X 437
+#define BOTTOM_BUTTON_Y 680
+#define GUI_SCALE 3
+#define MENU_X 232
+#define MENU_Y 256
+#define EXIT_X 0
+#define EXIT_Y 0
+#define MENU_OFFSET_X 232
+#define MENU_OFFSET_Y 256
+
+//Wall-focused macros 
+#define WALL_SPRITE_HEIGHT 32
+#define WALL_SPRITE_WIDTH 16
+#define WALL_SCALE 2
+#define GRID_SPACE 32
+
+#endif
---
+++ GameOff2025Submission/src/main.cpp
@@ -0,0 +1,69 @@
+// Wavebreak
+// 16x16 sprites at 320x180 is just way too good
+
+// coding, compiling, curating, coproducing, calculating, can-do attitude
+// big screen td game, similar to worldbox in scale and sprites
+// enemies, large-scale td game
+// waves of enemies visible
+
+// base-building, upgrades, simple pixel art
+
+// AAP-64 palette
+
+// Boss fight similar to undertale, but with tower defense as well
+
+// 6 waves,
+// the wave of light(green),
+// the wave of strength(pink),
+// the wave of speed(yellow),
+// the wave of time(brown),
+// the wave of evasion(red), and
+// the wave of defense(blue)
+
+// each wave has own attack
+
+// light: almost undodgable, low damage
+// strength: dodgable, but high damage
+// speed: undodgable, medium damage
+// time: buffs other waves with time powers, so save slotting, similar to with the flowey fight
+// evasion: allows boss and enemies to dodge your attacks
+// defense: blocks your attacks when active, decently easy to dodge
+#include "raylib.h"
+
+
+#include "gui.hpp"
+#include "player.hpp"
+
+
+#include "config.hpp"
+
+int main()
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "WAVEBREAK");
+    Image icon = LoadImage("gfx/icon/icon.png");
+    SetWindowIcon(icon);
+    UnloadImage(icon);
+    SetTargetFPS(60);
+
+    gui_init();
+    
+    gen_grid_coords();
+    
+    // Game Loop
+    while (WindowShouldClose() == false)
+    {
+        // Game Logic
+        
+        BeginDrawing();
+        DrawRectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
+        
+        //updates gui
+        gui_update();
+        
+        EndDrawing();
+    }
+    
+    CloseWindow();
+    
+    return 0;
+}
